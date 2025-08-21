9a072f440abf5eadd13f6c6bdbd9410d3be57310
R4-Rex <itsrex.dev@gmail.com>
2025-08-21 10:42:00 +0500

    Changed locale settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -31,15 +31,15 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "es_US.UTF-8";
-    LC_IDENTIFICATION = "es_US.UTF-8";
-    LC_MEASUREMENT = "es_US.UTF-8";
-    LC_MONETARY = "es_US.UTF-8";
-    LC_NAME = "es_US.UTF-8";
-    LC_NUMERIC = "es_US.UTF-8";
-    LC_PAPER = "es_US.UTF-8";
-    LC_TELEPHONE = "es_US.UTF-8";
-    LC_TIME = "es_US.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   # Configure keymap in X11
