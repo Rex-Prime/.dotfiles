3a4f839821e622a9e08977763a2e36b4992a6e0f
R4-Rex <itsrex.dev@gmail.com>
2025-11-02 17:49:11 +0000

    feat(memory): implement hybrid swap system with disk swap

--- modules/nixos/memory.nix
+++ modules/nixos/memory.nix
@@ -5,17 +5,26 @@
   zramSwap = {
     enable = true;
     memoryPercent = 25;  # 2GB of 8GB for zram
-    algorithm = "zstd";  # Best compression
+    algorithm = "zstd"; 
   };
 
 # Kernel memory parameters
   boot.kernelParams = [
     # Memory management
-    "vm.swappiness=10"          # Use swap less aggressively
+    "vm.swappiness=60"          # Use swap more(100)/less(0) aggressively
     "vm.vfs_cache_pressure=100" # Clear cache more aggressively
-    "vm.dirty_ratio=15"         # Write to disk sooner
+    "vm.dirty_ratio=20"         # Write to disk sooner
     "vm.dirty_background_ratio=5"
   ];
+
+# Disk Based Swap
+
+swapDevices = [
+{
+  device = "/swapfile";
+  size = 8 * 1024; # 8GB swap
+}
+];
 
   # Systemd memory limits for critical services
   systemd.extraConfig = ''
