fffa565df4d90f65add7b993bb59d168a4b6d087
R4-Rex <itsrex.dev@gmail.com>
2025-11-04 16:18:51 +0000

    perf(zsh): optimize shell configuration and plugins

--- modules/home-manager/zsh/sh.nix
+++ modules/home-manager/zsh/sh.nix
@@ -1,27 +1,60 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   myAliases = {
     ll = "ls -l";
     ".." = "cd ..";
+    la = "ls -la";
+    "..." = "cd ../..";
   };
 in
 {
-  # ZSH !!!
-  programs.zsh = {
-    enable = true;
-
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    autocd = true;
-
-    shellAliases = myAliases;
-
-    oh-my-zsh = {
+  programs = {
+    zsh = {
       enable = true;
-      plugins = [ "git" "z" "sudo" "colored-man-pages" "colorize" "history" "command-not-found" ];
+      
+      zprof.enable = false; # <-- For performance tests
+    
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      autocd = true;
+      shellAliases = myAliases;
+
+      oh-my-zsh = {
+      enable = true;
+      plugins = [ 
+        "git" 
+        "sudo" 
+        "colored-man-pages" 
+        "command-not-found"
+        "z"
+        "history"
+        ];
+      };
+
+      initContent = lib.mkMerge [
+      (lib.mkBefore ''
+      
+      # P10k instant prompt
+
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi 
+      '')
+      ''
+      # ZSH options for performance
+      setopt NO_BEEP
+      setopt NO_LIST_BEEP
+      
+      # History configuration
+      HISTSIZE=1000
+      SAVEHIST=1000
+      setopt HIST_IGNORE_ALL_DUPS
+      setopt HIST_SAVE_NO_DUPS
+    ''
+    ];
     };
   };
 }
