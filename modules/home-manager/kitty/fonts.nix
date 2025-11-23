099c7ef4dc9b9bde4b19b87c1f2813bf64ffb629
R4-Rex <itsrex.dev@gmail.com>
2025-11-23 06:46:48 +0000

    feat(kitty): switch font to JetBrainsMono Nerd Font

--- modules/home-manager/kitty/fonts.nix
+++ modules/home-manager/kitty/fonts.nix
@@ -2,9 +2,9 @@
 {
   programs.kitty.settings = {
   
-  font_family = "FiraCode Nerd Font";
+  font_family = "JetbrainsMono Nerd Font";
   
-  font_features = "FiraCodeNerdFontComplete-Reg +calt +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08"; #fyi calt+ enables ligeratures. and then its variants, +ss01 > +ss08 
+  font_features = "JetbriansMonoNF-Reg +calt +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08"; #fyi calt+ enables ligeratures. and then its variants, +ss01 > +ss08 
   
   };
 }
