340707f0d19aeabb6e298e3c770377eec1e2c282
R4-Rex <itsrex.dev@gmail.com>
2025-11-23 06:57:39 +0000

    fix(kitty): fix font_feature typo

--- modules/home-manager/kitty/fonts.nix
+++ modules/home-manager/kitty/fonts.nix
@@ -4,7 +4,7 @@
   
   font_family = "JetbrainsMono Nerd Font";
   
-  font_features = "JetbriansMonoNF-Reg +calt +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08"; #fyi calt+ enables ligeratures. and then its variants, +ss01 > +ss08 
+  font_features = "JetbrainsMonoNF-Reg +calt +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08"; #fyi calt+ enables ligeratures. and then its variants, +ss01 > +ss08 
   
   };
 }
