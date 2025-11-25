0261528cd330b3f41468e7f734779c2be5e34714
R4-Rex <itsrex.dev@gmail.com>
2025-11-25 09:08:58 +0000

    rofi(chore): added some comments

--- modules/home-manager/rofi/rofi.nix
+++ modules/home-manager/rofi/rofi.nix
@@ -14,11 +14,17 @@ in {
   
   };
 
-  xdg.dataFile."${themesDir}/style-1.rasi".source = ./themes/style-1.rasi;
+  /*****----- Themes -----*****/
   
+  xdg.dataFile."${themesDir}/style-1.rasi".source = ./themes/style-1.rasi;
+
+  /*****----- Shared -----*****/
+
   xdg.dataFile."${themesDir}/shared/colors.rasi".source = ./shared/colors.rasi;
   xdg.dataFile."${themesDir}/shared/fonts.rasi".source = ./shared/fonts.rasi;
   
+  /*****----- Color Schemes -----*****/
+
   xdg.dataFile."${themesDir}/colors/black.rasi".source = ./colors/black.rasi;
   xdg.dataFile."${themesDir}/colors/catppuccin.rasi".source = ./colors/catppuccin.rasi;
   xdg.dataFile."${themesDir}/colors/tokyonight.rasi".source = ./colors/tokyonight.rasi;
