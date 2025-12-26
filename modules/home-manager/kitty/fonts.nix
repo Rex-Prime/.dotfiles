a1a683508ba30851b4542823424c08225374f70a
R4-Rex <itsrex.dev@gmail.com>
2025-12-26 21:36:30 +0500

    feat(kitty): add 'block cursor' setting, and modularized a bit

--- modules/home-manager/kitty/fonts.nix
+++ modules/home-manager/kitty/fonts.nix
@@ -1,10 +1,23 @@
 {config, pkgs, ...}:
 {
-  programs.kitty.settings = {
-  
-  font_family = "JetbrainsMono Nerd Font";
-  
-  font_features = "JetbrainsMonoNF-Reg +calt +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08"; #fyi calt+ enables ligeratures. and then its variants, +ss01 > +ss08 
-  
+  programs.kitty = {
+
+    shellIntegration = {
+
+      mode = "no-cursor";
+
+    };
+
+    settings = {
+
+    # You can define all kitty configs inside here.. I think..
+
+      font_family = "JetbrainsMono Nerd Font";
+
+      font_features = "JetbrainsMonoNF-Reg +calt +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss08"; #fyi calt+ enables ligeratures. and then its variants, +ss01 > +ss08 
+
+      cursor_shape = "block";
+
+    };
   };
 }
