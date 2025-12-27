7505e23e5f70dd19cca2861ef2b4c32d0e54e4a2
R4-Rex <itsrex.dev@gmail.com>
2025-12-28 01:25:26 +0500

    refactor(zsh): symlink dotfile .zshrc to ~/.zshrc and made some suitable changes

--- modules/home-manager/zsh/sh.nix
+++ modules/home-manager/zsh/sh.nix
@@ -1,61 +1,11 @@
 { config, pkgs, lib, ... }:
 
-let
-  myAliases = {
-    ll = "ls -l";
-    ".." = "cd ..";
-    la = "ls -la";
-    "..." = "cd ../..";
-    "hm" = "home-manager";
-  };
-in
 {
-  programs = {
-    zsh = {
-      enable = true;
-      
-      zprof.enable = false; # <-- For performance tests
-    
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-
-      autocd = true;
-      shellAliases = myAliases;
-
-      oh-my-zsh = {
-      enable = true;
-      plugins = [ 
-        "git" 
-        "sudo" 
-        "colored-man-pages" 
-        "command-not-found"
-        "z"
-        "history"
-        ];
-      };
-
-      initContent = lib.mkMerge [
-      (lib.mkBefore ''
-      
-      # P10k instant prompt
-
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      fi 
-      '')
-      ''
-      # ZSH options for performance
-      setopt NO_BEEP
-      setopt NO_LIST_BEEP
-      
-      # History configuration
-      HISTSIZE=1000
-      SAVEHIST=1000
-      setopt HIST_IGNORE_ALL_DUPS
-      setopt HIST_SAVE_NO_DUPS
-    ''
-    ];
-    };
-  };
+  home.packages = [ pkgs.zoxide ];
+  
+    home.activation.linkZshrc = config.lib.dag.entryAfter ["writeBoundary"] ''
+    if [ ! -L "$HOME/.zshrc" ]; then
+      ln -sf "$HOME/.dotfiles/modules/home-manager/zsh/.zshrc" "$HOME/.zshrc"
+    fi
+    '';
 }
