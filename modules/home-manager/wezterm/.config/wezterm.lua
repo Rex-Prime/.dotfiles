8bb6aa5ebd12e734f9f3bc38b0ab2dd3538808c5
Rex-Prime <itsrex.dev@gmail.com>
2026-04-20 10:20:10 +0500

    wezterm: add ctrl+click keybind

--- modules/home-manager/wezterm/.config/wezterm.lua
+++ modules/home-manager/wezterm/.config/wezterm.lua
@@ -32,7 +32,15 @@ config = {
                         mods = "CTRL|SHIFT",
                         action = wezterm.action.ToggleFullScreen,
                 },
-        }
+        },
+        mouse_bindings = {
+                -- Ctrl-click will open the link under the mouse cursor
+                {
+                        event = { Up = { streak = 1, button = "Left" } },
+                        mods = "CTRL",
+                        action = wezterm.action.OpenLinkAtMouseCursor,
+                },
+        },
 }
 -- Finally, return the configuration to wezterm:
 return config
