79e4a5ae8c68f72101b293d4a9b4a9ebc1b78a98
Rex-Prime <itsrex.dev@gmail.com>
2026-04-20 09:53:59 +0500

    wezterm: improved config format

--- modules/home-manager/wezterm/.config/wezterm.lua
+++ modules/home-manager/wezterm/.config/wezterm.lua
@@ -7,16 +7,24 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 128
-config.initial_rows = 32
+config = {
+        initial_cols = 128,
+        initial_rows = 32,
 
-config.enable_tab_bar = false
+        enable_tab_bar = false,
 
--- or, changing the font size and color scheme.
-config.font_size = 14
-config.font = wezterm.font ('JetBrainsMonoNL Nerd Font Mono', {weight="Regular", stretch="Normal", style="Normal"})
-config.color_scheme = 'rose-pine-moon'
-config.color_scheme_dirs = {'./colors/'}
-
+        -- or, changing the font size and color scheme.
+        font_size = 14,
+        font = wezterm.font (
+                'JetBrainsMonoNL Nerd Font Mono',
+                {
+                        weight="Regular",
+                        stretch="Normal",
+                        style="Normal"
+                }
+        ),
+        color_scheme = 'rose-pine-moon',
+        color_scheme_dirs = {'./colors/'},
+}
 -- Finally, return the configuration to wezterm:
 return config
