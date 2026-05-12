c32d1ea8dc18be36185cd6ca79f04c3cd6236349
Rex-Prime <itsrex.dev@gmail.com>
2026-05-12 16:14:59 +0500

    nvim: added stylua config & symlinked it

--- modules/home-manager/neovim/nvim.nix
+++ modules/home-manager/neovim/nvim.nix
@@ -1,6 +1,12 @@
-{ pkgs, neonix, ... }:
+{
+  pkgs,
+  config,
+  neonix,
+  ...
+}:
 {
   home.packages = [
     neonix.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
+  xdg.configFile."stylua/stylua.toml".source = ./stylua.toml;
 }
