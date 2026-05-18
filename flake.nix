bc99f86b301558422e696bdba9d8e74f7d4668f1
Rex-Prime <itsrex.dev@gmail.com>
2026-05-18 20:57:09 +0500

    flake: formatted flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,19 @@
-
 {
   description = "My Fleks";
 
   inputs = {
-    
+
     # Nix Packages!
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    
+
     # Home-Manager!
     home-manager = {
-    url = "github:nix-community/home-manager/release-25.11";
-    inputs.nixpkgs.follows = "nixpkgs"; # makes sure both nixpkgs reference is the same
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs"; # makes sure both nixpkgs reference is the same
     };
 
     neonix = {
-    url = "github:Rex-Prime/neonix-wrapper";
+      url = "github:Rex-Prime/neonix-wrapper";
     };
 
     # My Vars!
@@ -23,86 +22,90 @@
       url = "path:///home/rex/.vars";
 
     };
-    
+
   };
 
-  outputs = { nixpkgs, home-manager, myvars, neonix, ... } @inputs:
-
-  let
-    systemSettings = {
-      system = "x86_64-linux";
-      hostname = "potato";
-      locale = "en_US.UTF-8";
-    };
-    
-    userSettings = {
-      username = "rex"; 
-      term = "kitty";
-      editor = "nvim";
-    };
-    
-    vars = myvars.secrets;
-
-    lib = nixpkgs.lib;
-    system = systemSettings.system;
-    pkgs = nixpkgs.legacyPackages.${system};
-    
-    # Defines the System
-    potato = lib.nixosSystem {
-     
-     inherit system;
-
-     specialArgs = { inherit systemSettings userSettings vars; };
-
-     modules = [ 
-	
-       ./nixos/configuration.nix
-	
-     ];
-    };
-
-
-in {
-
-    #  NIXOS SYSTEM CONFIGURATION
-    # Command: sudo nixos-rebuild switch --flake . Or .#potato
-    
-    nixosConfigurations = {
-
-    "${systemSettings.hostname}" = potato; # normally entire thing was here, but I made it cleaner :))
-    
-    # Make potato the default for everything :))
-    default = potato;
-    
-    # Normally the rebuild command looks for 'nixos' instead of the 'default' I defined above
-    # So.. I point the system in the right direction :))
-
-	  nixos = potato; # this is also potato, just in case :))
-
-};
-    # STANDALONE HOME MANAGER CONFIGURATION (FLAKE-BASED) 
-	  # Command: home-manager switch --flake #user
-
-     homeConfigurations."${userSettings.username}" = home-manager.lib.homeManagerConfiguration 
+  outputs =
     {
-      pkgs = pkgs;
+      nixpkgs,
+      home-manager,
+      myvars,
+      neonix,
+      ...
+    }@inputs:
 
-      extraSpecialArgs = 
-      { 
-        inherit systemSettings userSettings; 
-        inherit inputs; # Pass inputs if needed in home.nix
-        inherit vars;
-	inherit neonix;
+    let
+      systemSettings = {
+        system = "x86_64-linux";
+        hostname = "potato";
+        locale = "en_US.UTF-8";
       };
 
-      modules = [
-        
-        # Home-manager module
-        ./home.nix
-        #...
-      ];
+      userSettings = {
+        username = "rex";
+        term = "kitty";
+        editor = "nvim";
+      };
+
+      vars = myvars.secrets;
+
+      lib = nixpkgs.lib;
+      system = systemSettings.system;
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      # Defines the System
+      potato = lib.nixosSystem {
+
+        inherit system;
+
+        specialArgs = { inherit systemSettings userSettings vars; };
+
+        modules = [
+
+          ./nixos/configuration.nix
+
+        ];
+      };
+
+    in
+    {
+
+      #  NIXOS SYSTEM CONFIGURATION
+      # Command: sudo nixos-rebuild switch --flake . Or .#potato
+
+      nixosConfigurations = {
+
+        "${systemSettings.hostname}" = potato; # normally entire thing was here, but I made it cleaner :))
+
+        # Make potato the default for everything :))
+        default = potato;
+
+        # Normally the rebuild command looks for 'nixos' instead of the 'default' I defined above
+        # So.. I point the system in the right direction :))
+
+        nixos = potato; # this is also potato, just in case :))
+
+      };
+      # STANDALONE HOME MANAGER CONFIGURATION (FLAKE-BASED)
+      # Command: home-manager switch --flake #user
+
+      homeConfigurations."${userSettings.username}" = home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgs;
+
+        extraSpecialArgs = {
+          inherit systemSettings userSettings;
+          inherit inputs; # Pass inputs if needed in home.nix
+          inherit vars;
+          inherit neonix;
+        };
+
+        modules = [
+
+          # Home-manager module
+          ./home.nix
+          #...
+        ];
+      };
     };
-  };
 
 }
-
