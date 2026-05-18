2844035a9059ce11ee548cbe37ff31e0006efe0e
Rex-Prime <itsrex.dev@gmail.com>
2026-05-18 21:55:14 +0500

    feat: symlinked wallpapers & load them in hyprpaper

--- modules/home-manager/hypr/hyprpaper.nix
+++ modules/home-manager/hypr/hyprpaper.nix
@@ -1,8 +1,18 @@
-{config, pkgs, ...}:
+{
+  config,
+  pkgs,
+  lib,
+  userSettings,
+  ...
+}:
 
 {
   home.packages = with pkgs; [
     hyprpaper
   ];
 
+  home.file."wallpapers" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${userSettings.dotpath}/wallpapers/";
+    recursive = true;
+  };
 }
