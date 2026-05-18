c4461c308d450fc412cdca526bf72c2672dba796
Rex-Prime <itsrex.dev@gmail.com>
2026-05-18 22:17:35 +0500

    refactor: change dotfile path to userSettings.dotpath

--- modules/home-manager/utils/fastfetch/fastfetch.nix
+++ modules/home-manager/utils/fastfetch/fastfetch.nix
@@ -1,11 +1,16 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  userSettings,
+  ...
+}:
 {
   programs.fastfetch = {
-    
+
     enable = true;
 
-    };
+  };
 
-  xdg.configFile."fastfetch".source = 
-      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/modules/home-manager/utils/fastfetch/config/";
+  xdg.configFile."fastfetch".source =
+    config.lib.file.mkOutOfStoreSymlink "${userSettings.dotpath}/modules/home-manager/utils/fastfetch/config/";
 }
